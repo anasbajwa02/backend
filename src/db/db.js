b973abc7fab2d219f1853e5e464369c7f71d542a
anasbajwa02 <anas03154297874@gmail.com>
2025-09-02 12:31:20 +0500

    minor issues

--- src/db/db.js
+++ src/db/db.js
@@ -6,7 +6,7 @@ import { DB_NAME } from "../constants.js";
 
 const connectDB = async ()=>{
 try{
-const connectionIstance =   await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+const connectionIstance =   await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`)
    console.log(`\n MOngoDb connected Seccessfully!! and DB host is ${connectionIstance.connection.host}`)
   
 
